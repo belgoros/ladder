97f6ceb879e89434260ba9c39a140cb3d1fb387f
belgoros <s.cambour@gmail.com>
2026-02-23 14:49:57 +0100

    Add Words.move/1

--- lib/ladder/words.ex
+++ lib/ladder/words.ex
@@ -3,4 +3,12 @@ defmodule Ladder.Words do
   @word_index Ladder.Dictionary.index_words(@words)
 
   def random_word, do: Enum.random(@words)
+
+  def move(word) do
+    word
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {char, position} -> @word_index[{char, position}] end)
+    |> Enum.random()
+  end
 end
