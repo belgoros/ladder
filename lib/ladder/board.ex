1483637ecd94a0a2f94dfd456c78420bc8efc011
belgoros <s.cambour@gmail.com>
2026-02-23 12:03:11 +0100

    Add Board.new/1

--- lib/ladder/board.ex
+++ lib/ladder/board.ex
@@ -1,4 +1,17 @@
 defmodule Ladder.Board do
+  defstruct [:answer, :moves]
   @words Ladder.Dictionary.words()
   @word_index Ladder.Dictionary.index_words(@words)
+
+  def new({initial_word, answer}) do
+    %__MODULE__{answer: answer, moves: [initial_word]}
+  end
+
+  def turns(board, word) do
+    # .. word
+  end
+
+  def show(board) do
+    # show a bord to the user
+  end
 end
