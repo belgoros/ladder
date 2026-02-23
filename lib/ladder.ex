3088834ef6a51204a7c79728840228b150795182
belgoros <s.cambour@gmail.com>
2026-02-23 09:54:49 +0100

    Initial commit

---
+++ lib/ladder.ex
@@ -0,0 +1,18 @@
+defmodule Ladder do
+  @moduledoc """
+  Documentation for `Ladder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ladder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ladder_test.exs
@@ -0,0 +1,8 @@
+defmodule LadderTest do
+  use ExUnit.Case
+  doctest Ladder
+
+  test "greets the world" do
+    assert Ladder.hello() == :world
+  end
+end
