91b0e3c8c9cb4cdb988d4966c6bca5f70b02f5a6
belgoros <s.cambour@gmail.com>
2026-02-23 10:26:31 +0100

    Remove generated code in Ladder module

--- lib/ladder.ex
+++ lib/ladder.ex
@@ -1,18 +1,2 @@
 defmodule Ladder do
-  @moduledoc """
-  Documentation for `Ladder`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ladder.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
